0cb996422956a0821b7d702efdfe044ec7e851d5
nicholasfarone <nicholas.farone@snhu.edu>
2021-10-03 16:09:03 -0400

    MainFeed + settings

--- app/src/main/java/com/example/projectjse/ProfileActivity.java
+++ app/src/main/java/com/example/projectjse/ProfileActivity.java
@@ -7,37 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainFeedActivity extends AppCompatActivity {
-    private Button setting;
-    private Button profile;
+public class ProfileActivity extends AppCompatActivity {
+    private Button Back;
+    private Button logOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main_feed);
-        setting = (Button) findViewById(R.id.settingsButton);
-        setting.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_profile);
+        Back = (Button) findViewById(R.id.BackButtonPro);
+        Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity();
             }
-
         });
-        profile = (Button) findViewById(R.id.profileButton);
-        profile.setOnClickListener(new View.OnClickListener() {
+        logOut = (Button) findViewById(R.id.logOutButton);
+        logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
-
     }
     public void openActivity(){
-        Intent intent = new Intent(this, Settings.class);
+        Intent intent = new Intent(this, MainFeedActivity.class);
         startActivity(intent);
-    }
-    public void openActivity2(){
-        Intent intent = new Intent(this, ProfileActivity.class);
+    }public void openActivity2(){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
